bf1536117fb9485d65c4a499281d254e39b91d01
dinoru <diarra.msa@gmail.com>
2025-06-19 16:38:48 +0300

    home component

--- dashboard/src/components/Home.jsx
+++ dashboard/src/components/Home.jsx
@@ -125,7 +125,7 @@ function Home() {
 
             <button
               onClick={logout}
-              className="bg-re text-red-600 px-4 py-2 rounded-lg flex items-center gap-2 hover:bg-red-50 transition-colors"
+              className="bg-red-600 text-white px-4 py-2 rounded-lg flex items-center gap-2 hover:bg-red-50 transition-colors"
             >
               <FaSignOutAlt />
               <span className="hidden md:inline">Выход</span>
